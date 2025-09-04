35e2eb6a6f940b0033fd609c627d783c57a08305
Gaurav Joshi <gaurav9997961651jjk277584@outlook.com>
2025-09-04 20:52:53 +0530

    Well Added More colour for better look and also create a pipleline for the user to select the formate of the ppt for now there is just one and other is a fallback which is basic but in future there will be more as also fix a single slide bug

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from converter.excel_reader import read_excel_1
 from converter.ppt_writer import create_ppt,add_table_slide,save_ppt
+from converter.table_router import add_smart_table_slides
+from converter.format_tables import add_dataframe_table_slide
 import os
 import pandas as pd
 def main():
@@ -20,11 +22,11 @@ def main():
     
 
     ## Slide 1:full Portfolio
-    prs=add_table_slide(prs,df,title="Full portfoilio Allocation")
+    prs=add_smart_table_slides(prs,df,title="Full portfoilio Allocation")
 
     ## Slide 2: Top # assests ONly
     df_top3=df.head(3) ## First 3 rows
-    prs=add_table_slide(prs,df_top3,title=" Top 3 Portfoilio Assests")
+    prs=add_smart_table_slides(prs,df_top3,title=" Top 3 Portfoilio Assests")
 
     ## Save the PPT
     save_ppt(prs, "examples/demo_PPT/output_demo.pptx")
